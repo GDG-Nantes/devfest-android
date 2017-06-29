ce5f81305622031aa5d7eef630e430655f05a794
Cyril Mottier <cyrilmottier@gmail.com>
2017-06-29 14:07:31 +0200

    Use preview theme variants for Activity

--- app/src/main/kotlin/com/gdgnantes/devfest/android/app/BaseActivity.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/app/BaseActivity.kt
@@ -2,9 +2,10 @@ package com.gdgnantes.devfest.android.app
 
 import android.arch.lifecycle.LifecycleRegistry
 import android.arch.lifecycle.LifecycleRegistryOwner
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.gdgnantes.devfest.android.BuildConfig
-
+import com.gdgnantes.devfest.android.util.ThemeUtils
 
 abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
 
@@ -14,7 +15,11 @@ abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
 
     private val registry = LifecycleRegistry(this)
 
-    override fun getLifecycle(): LifecycleRegistry = registry
+    override fun onCreate(inState: Bundle?) {
+        ThemeUtils.ensureRuntimeTheme(this)
+        super.onCreate(inState)
+    }
 
+    override fun getLifecycle(): LifecycleRegistry = registry
 
 }
