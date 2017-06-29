c1abae8c420c995c00fcfe1058f2f5567a3185c2
Cyril Mottier <cyrilmottier@gmail.com>
2017-06-29 16:40:01 +0200

    Associate icons & names to each SocialNetworks

--- app/src/main/kotlin/com/gdgnantes/devfest/android/model/SocialNetwork.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/model/SocialNetwork.kt
@@ -2,7 +2,7 @@ package com.gdgnantes.devfest.android.model
 
 import android.content.Context
 import android.graphics.drawable.Drawable
-import android.support.v4.content.ContextCompat
+import android.support.v7.content.res.AppCompatResources
 import com.gdgnantes.devfest.android.R
 
 enum class SocialNetwork(
@@ -10,10 +10,22 @@ enum class SocialNetwork(
         private val icon: Int,
         private val networkName: Int) {
 
-    Twitter("twitter", R.drawable.notification_icon_background, R.string.app_name),
-    GooglePlus("gplus", R.drawable.notification_icon_background, R.string.app_name),
-    Website("website", R.drawable.notification_icon_background, R.string.app_name),
-    GitHub("github", R.drawable.notification_icon_background, R.string.app_name);
+    Twitter(
+            "twitter",
+            R.drawable.ic_network_twitter,
+            R.string.network_twitter),
+    GooglePlus(
+            "google_plus",
+            R.drawable.ic_network_google_plus,
+            R.string.network_google_plus),
+    Website(
+            "website",
+            R.drawable.ic_network_web,
+            R.string.network_website),
+    GitHub(
+            "github",
+            R.drawable.ic_network_github,
+            R.string.network_github);
 
     companion object {
         fun get(apiValue: String): SocialNetwork? {
@@ -21,7 +33,7 @@ enum class SocialNetwork(
         }
     }
 
-    fun getIcon(context: Context): Drawable = ContextCompat.getDrawable(context, icon)
+    fun getIcon(context: Context): Drawable = AppCompatResources.getDrawable(context, icon)!!
 
     fun getNetworkName(context: Context): String = context.getString(networkName)
 }
