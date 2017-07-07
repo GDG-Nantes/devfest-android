0da7ef85e501af7c705448ef0edef2727e3c2004
Cyril Mottier <cyrilmottier@gmail.com>
2017-07-07 10:00:06 +0200

    Add PREFIX_ACTION

--- app/src/main/kotlin/com/gdgnantes/devfest/android/app/ContextExtensions.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/app/ContextExtensions.kt
@@ -2,4 +2,5 @@ package com.gdgnantes.devfest.android.app
 
 import com.gdgnantes.devfest.android.BuildConfig
 
+const val PREFIX_ACTION = "${BuildConfig.APPLICATION_ID}.action."
 const val PREFIX_EXTRA = "${BuildConfig.APPLICATION_ID}.extra."
