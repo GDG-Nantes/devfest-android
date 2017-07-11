e24096e63260cf96f8e1d60be8bd57ced2ea871a
Cyril Mottier <cyrilmottier@gmail.com>
2017-07-11 10:17:44 +0200

    Sync whenever the process gets visible

--- app/src/main/kotlin/com/gdgnantes/devfest/android/ScheduleApplication.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/ScheduleApplication.kt
@@ -3,13 +3,16 @@ package com.gdgnantes.devfest.android
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.ProcessLifecycleOwner
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatDelegate
 import com.gdgnantes.devfest.android.content.RemindersManager
 import com.gdgnantes.devfest.android.util.BuildUtils
-
 
 class ScheduleApplication : Application() {
 
@@ -19,6 +22,7 @@ class ScheduleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(ProcessLifecycleObserver())
         if (BuildUtils.hasO()) {
             createNotificationChannels()
         }
@@ -36,6 +40,13 @@ class ScheduleApplication : Application() {
             vibrationPattern = longArrayOf(500, 500, 500, 500)
         }
         getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+    }
+
+    internal inner class ProcessLifecycleObserver : LifecycleObserver {
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun onStart() {
+            SyncService.sync(this@ScheduleApplication)
+        }
     }
 
 }
