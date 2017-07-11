c59db0ae121c515f8f1870146b72f289f4d39773
Cyril Mottier <cyrilmottier@gmail.com>
2017-07-11 18:17:44 +0200

    Make sure fields in SocialLink are properties

--- app/src/main/kotlin/com/gdgnantes/devfest/android/model/SocialLink.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/model/SocialLink.kt
@@ -3,6 +3,7 @@ package com.gdgnantes.devfest.android.model
 import android.support.annotation.Keep
 
 @Keep
-class SocialLink(
-        @JvmField val network: SocialNetwork? = null,
-        @JvmField val url: String? = null)
+class SocialLink {
+    @JvmField val network: SocialNetwork? = null
+    @JvmField val url: String? = null
+}
