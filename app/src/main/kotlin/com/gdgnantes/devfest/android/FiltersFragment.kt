ee860e3928b2f7f822fb96ff1efb7fba75ea3c9e
Cyril Mottier <cyrilmottier@gmail.com>
2017-07-23 09:48:36 +0200

    Add a new “Filters” panel on MainActivity

---
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/FiltersFragment.kt
@@ -0,0 +1,11 @@
+package com.gdgnantes.devfest.android
+
+import com.gdgnantes.devfest.android.app.BaseFragment
+
+class FiltersFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance(): FiltersFragment = FiltersFragment()
+    }
+
+}
