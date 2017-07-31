1a1f0eebd5e9c98c72c40c7f06d6ef49d56b30c9
Cyril Mottier <cyrilmottier@gmail.com>
2017-07-31 07:18:25 +0200

    Bump application version

--- app/src/main/kotlin/com/gdgnantes/devfest/android/app/Build.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/app/Build.kt
@@ -3,9 +3,10 @@ package com.gdgnantes.devfest.android.app
 object Build {
 
     enum class Versions(val versionCode: Int) {
-        ANGERS(100)
+        ANGERS(100),
+        ANGERS_MR1(101)
     }
 
-    val version = Versions.ANGERS
+    val version = Versions.ANGERS_MR1
 
 }
