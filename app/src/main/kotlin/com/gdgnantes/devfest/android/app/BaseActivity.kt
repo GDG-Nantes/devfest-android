33e896fb499167d6abc54d016470b320d7f28926
Cyril Mottier <cyrilmottier@gmail.com>
2017-09-25 07:22:02 +0200

    Bump project’s libraries

--- app/src/main/kotlin/com/gdgnantes/devfest/android/app/BaseActivity.kt
+++ app/src/main/kotlin/com/gdgnantes/devfest/android/app/BaseActivity.kt
@@ -1,22 +1,16 @@
 package com.gdgnantes.devfest.android.app
 
-import android.arch.lifecycle.LifecycleRegistry
-import android.arch.lifecycle.LifecycleRegistryOwner
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.gdgnantes.devfest.android.util.ThemeUtils
 
-abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
-
-    private val registry = LifecycleRegistry(this)
+abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(inState: Bundle?) {
         ThemeUtils.ensureRuntimeTheme(this)
         super.onCreate(inState)
     }
-
-    override fun getLifecycle(): LifecycleRegistry = registry
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> {
